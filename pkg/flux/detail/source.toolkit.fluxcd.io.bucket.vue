a523e3f253f455db8f6d0e0d1d75b3e34478cfd8
mqufflc <94301322+mqufflc@users.noreply.github.com>
2025-08-31 18:22:45 +0200

    Add empty detail pages

---
+++ pkg/flux/detail/source.toolkit.fluxcd.io.bucket.vue
@@ -0,0 +1,25 @@
+<script>
+import CreateEditView from '@shell/mixins/create-edit-view';
+import Tab from '@shell/components/Tabbed/Tab';
+import ResourceTabs from '@shell/components/form/ResourceTabs';
+import RelatedResources from '@shell/components/RelatedResources';
+import InventoryResources from '../components/InventoryResources.vue';
+
+export default {
+  name: 'BucketDetail',
+
+  components: { ResourceTabs, Tab, RelatedResources, InventoryResources },
+
+  mixins: [CreateEditView],
+};
+</script>
+
+<template>
+  <ResourceTabs
+    :mode="mode"
+    :value="value"
+    :needRelated="false"
+  >
+    
+  </ResourceTabs>
+</template>
