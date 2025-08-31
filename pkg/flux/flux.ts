511d9d27a039dec33142210d358e3c72a3680bdd
mqufflc <94301322+mqufflc@users.noreply.github.com>
2025-08-31 18:19:51 +0200

    Fix action display on events table

--- pkg/flux/flux.ts
+++ pkg/flux/flux.ts
@@ -18,13 +18,13 @@ export function init($plugin: IPlugin, store: any) {
     ifHaveGroup: 'source.toolkit.fluxcd.io',
     weight:  50,
     showNamespaceFilter: true,
-    to:      {
-      name:   `c-cluster-${ FLUX_PRODUCT_NAME }-${ FLUX_DASHBOARD_PAGE_NAME }`,
-      params: { product: FLUX_PRODUCT_NAME },
-      meta: {
-        product: FLUX_PRODUCT_NAME
-      }
-    }
+    // to:      {
+    //   name:   `c-cluster-${ FLUX_PRODUCT_NAME }-${ FLUX_DASHBOARD_PAGE_NAME }`,
+    //   params: { product: FLUX_PRODUCT_NAME },
+    //   meta: {
+    //     product: FLUX_PRODUCT_NAME
+    //   }
+    // }
   });
 
   configureType(KUSTOMIZATION_RESOURCE_NAME, {
